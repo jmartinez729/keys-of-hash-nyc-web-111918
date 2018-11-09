7017294c9abc80ca2842b4e9ad54c926eaa0c15a
John Martinez <jmartinez729@gmail.com>
2018-11-09 18:14:28 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,10 +1,6 @@
 class Hash
   def keys_of(arguments)
     array = []
-    if arguments = "Panama"
-      animals = ["red-footed tortoise"]
-      elsif arguments = "Madagascar"
-      animals = ["red-footed tortoise", "aye-aye"]
-    end 
+    
   end
 end
